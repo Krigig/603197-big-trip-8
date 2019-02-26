4a9bcfd742bb7196f947ff9d448e33f88338adbe
Kristina <vasileva_kri@list.ru>
2019-02-26 20:19:16 +0200

    Доработка рабочего процесса

--- src/main.js
+++ src/main.js
@@ -7,12 +7,32 @@ filters.insertAdjacentHTML(`beforeend`, makeFilter(`Everything`, true));
 filters.insertAdjacentHTML(`beforeend`, makeFilter(`Future`));
 filters.insertAdjacentHTML(`beforeend`, makeFilter(`Past`));
 
-const renderTripPoint = (dist) => {
-  const tasks = new Array(7)
+const renderTripPoint = (amount, dist) => {
+  const tasks = new Array(amount)
     .fill()
     .map(makeTripPoint);
   dist.insertAdjacentHTML(`beforeend`, tasks.join(``));
 };
 
 const tripPointsContainer = document.querySelector(`.trip-day__items`);
-renderTripPoint(tripPointsContainer);
+renderTripPoint(7, tripPointsContainer);
+
+const filtersArray = filters.querySelectorAll(`input`);
+filtersArray.forEach(function (element) {
+  element.addEventListener(`click`, function () {
+    removeTripPoint();
+    let amountTripPoins = Math.round(Math.random() * 10);
+    renderTripPoint(amountTripPoins, tripPointsContainer);
+  });
+});
+
+const removeTripPoint = function () {
+  const tripPointsArray = document.querySelectorAll(`.trip-point`);
+  if (tripPointsArray.length !== 0) {
+    tripPointsArray.forEach(function (element) {
+      tripPointsContainer.removeChild(element);
+    });
+  }
+};
+
+
